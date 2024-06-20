b2530953f92033ff3b1d9b382760c78568967a77
rafly28 <raflyfirdaus28@gmail.com>
2024-06-20 16:04:42 +0700

    Pertamax

---
+++ schema.ts
@@ -0,0 +1,49 @@
+// Welcome to your schema
+//   Schema driven development is Keystone's modus operandi
+//
+// This file is where we define the lists, fields and hooks for our data.
+// If you want to learn more about how lists are configured, please read
+// - https://keystonejs.com/docs/config/lists
+
+import { list } from "@keystone-6/core";
+import { allowAll } from "@keystone-6/core/access";
+
+// see https://keystonejs.com/docs/fields/overview for the full list of fields
+//   this is a few common fields for an example
+import {
+  text,
+  relationship,
+  password,
+  timestamp,
+  select,
+} from "@keystone-6/core/fields";
+
+// the document field is a more complicated field, so it has it's own package
+import { document } from "@keystone-6/fields-document";
+// if you want to make your own fields, see https://keystonejs.com/docs/guides/custom-fields
+
+// when using Typescript, you can refine your types to a stricter subset by importing
+// the generated types from '.keystone/types'
+import type { Lists } from ".keystone/types";
+
+export const lists: Lists = {
+  User: list({
+    access: allowAll,
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      email: text({
+        validation: { isRequired: true },
+        isIndexed: "unique",
+      }),
+      password: password({ validation: { isRequired: true } }),
+      role: select({
+        options: [
+          { label: "Admin", value: "admin" },
+          { label: "Members", value: "members" },
+        ],
+      }),
+      department: relationship({ ref: "Department.members", many: false }),
+      taks: relationship({ ref: "Task.assignedTo", many: true }),
+    },
+  }),
+};
